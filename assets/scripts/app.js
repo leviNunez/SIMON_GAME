c1115eff9f0fdd459a343dea6ac0686821edeba9
leviNunez <levi.jupiter@gmail.com>
2021-03-30 23:17:27 -0400

    MOVED LED LIGHT TO A DIFFERENT POSITION AND FIXED BUG WITH THE TOGGLE BUTTON

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,14 @@
-const powerBtn = document.getElementById('power-btn');
-const startBtn = document.getElementById('start-btn');
-const resetBtn = document.getElementById('reset-btn');
-const displayText = document.getElementById('display-text');
-const toggleSwitch = document.querySelector('input');
-const ledLight = document.getElementById('led-light');
-const coloredBtnsArr = ['green', 'red', 'yellow', 'blue'];
+const powerBtn = document.getElementById("power-btn");
+const startBtn = document.getElementById("start-btn");
+const resetBtn = document.getElementById("reset-btn");
+const displayText = document.getElementById("display-text");
+const toggleSwitch = document.querySelector("input");
+const ledLight = document.getElementById("led-light");
+
+const coloredBtnsArr = ["green", "red", "yellow", "blue"];
+const correctAnswer = ["CORRECT!", "EXCELLENT!", "AWESOME!", "PERFECT!"];
+const levelText = "LEVEL: ";
+const pressStart = "PRESS START TO BEGING";
 
 let isPowerOn = false;
 let isGameStarted = false;
@@ -14,17 +18,15 @@ let functionStopper = false;
 let gamePattern = [];
 let playerPattern = [];
 let currentLevel = 0;
-let displayLevel = 'LEVEL:<span id="display-number">0</span>';
 let count;
-
-
 
 //MAIN FUNCTIONS---------------------------------------------------------------------
 
 //POWER ON/OFF
-powerBtn.addEventListener('click', function () {
+powerBtn.addEventListener("click", function () {
   if (isPowerOn === false) {
     powerOnAnimation(1500);
+    turnLedLightOn("green");
     setTimeout(function () {
       isPowerOn = true;
     }, 1500);
@@ -33,14 +35,15 @@ powerBtn.addEventListener('click', function () {
     setTimeout(function () {
       isPowerOn = false;
       resetGame();
+      turnLedLightOff();
     }, 1500);
   }
 });
 
 //STARTS THE GAME
 
-startBtn.addEventListener('click', function () {
-  if (displayText.innerHTML === 'PRESS START TO BEGIN') {
+startBtn.addEventListener("click", function () {
+  if (displayText.innerHTML === pressStart) {
     isGameStarted = true;
     functionStopper = false;
     setDefaultDisplayText();
@@ -50,24 +53,23 @@ startBtn.addEventListener('click', function () {
 
 //RESETS THE GAME
 
-resetBtn.addEventListener('click', function () {
+resetBtn.addEventListener("click", function () {
   if (isGameStarted) {
     functionStopper = true;
-    displayText.innerHTML = 'RESETTING...';
-    setTimeout(function() {
+    displayText.innerHTML = "RESETTING...";
+    setTimeout(function () {
       resetGame();
-      displayText.innerHTML = 'PRESS START TO BEGIN';
-    }, 500); 
+      displayText.innerHTML = pressStart;
+    }, 500);
   }
 });
-
 
 //HANDLES COMPUTER'S SEQUENCE
 
 function gameSequence() {
   playersTurn = false;
-  turnLedLightOn('yellow');
-  displayText.innerHTML = 'WATCH!';
+  turnLedLightOn("yellow");
+  displayText.innerHTML = "WATCH!";
   currentLevel++;
   playerPattern = [];
   const randomNumber = Math.floor(Math.random() * 4);
@@ -81,17 +83,19 @@ function gameSequence() {
     }
     setDefaultDisplayText();
     playersTurn = true;
-    turnLedLightOn('green');
+    turnLedLightOn("green");
   }, 1000 * count);
 }
 
 //HANDLES PLAYER'S SEQUENCE
 
 for (let i = 0; i <= 3; i++) {
-  document.querySelectorAll('.colored-btn')[i].addEventListener('click', function () {
+  document
+    .querySelectorAll(".colored-btn")
+    [i].addEventListener("click", function () {
       if (playersTurn === true) {
         const clickedBtn = this.id;
-        makeAButtonFlash(clickedBtn, 600);
+        makeAButtonFlash(clickedBtn, 400);
         playASound(clickedBtn);
         playerPattern.push(this.id);
         let index = playerPattern.length - 1;
@@ -109,7 +113,7 @@ for (let i = 0; i <= 3; i++) {
 function normalCheck(index) {
   if (playerPattern[index] === gamePattern[index]) {
     if (playerPattern.length === gamePattern.length) {
-      displayText.innerHTML = 'CORRECT!';
+      displayText.innerHTML = "CORRECT!";
       turnLedLightOff();
       setTimeout(function () {
         gameSequence();
@@ -118,12 +122,12 @@ function normalCheck(index) {
   } else {
     playersTurn = false;
     playerPattern = [];
-    playASound('wrong');
-    makeLedLightFlash('red', 1000);
-    displayText.innerHTML = 'WRONG!';
+    playASound("wrong");
+    makeLedLightFlash("red", 1000);
+    displayText.innerHTML = "WRONG!";
     setTimeout(function () {
-      displayText.innerHTML = 'WATCH!';
-      turnLedLightOn('yellow');
+      displayText.innerHTML = "WATCH!";
+      turnLedLightOn("yellow");
     }, 1000);
     setTimeout(function () {
       coloredBtnsAnimation(count);
@@ -134,7 +138,7 @@ function normalCheck(index) {
       }
       setDefaultDisplayText();
       playersTurn = true;
-      turnLedLightOn('green');
+      turnLedLightOn("green");
     }, 1000 * count);
   }
 }
@@ -142,63 +146,59 @@ function normalCheck(index) {
 function strictCheck(index) {
   if (playerPattern[index] === gamePattern[index]) {
     if (playerPattern.length === gamePattern.length) {
-      displayText.innerHTML = 'CORRECT!';
+      displayText.innerHTML = "CORRECT!";
       turnLedLightOff();
       setTimeout(function () {
         gameSequence();
       }, 1000);
     }
   } else {
-    playersTurn= false;
-    playASound('wrong');
+    playersTurn = false;
+    playASound("wrong");
     gameOver();
-    setTimeout(function() {
+    setTimeout(function () {
       resetGame();
     }, 1000);
-
-  }  
-    
+  }
 }
 
 //COLORED BUTTON SEQUENCE
 const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 async function coloredBtnsAnimation(count) {
-  let i = 0;
   let color;
-  while (i < count) {
+  for (let i = 0; i < count; i++) {
     if (functionStopper === true) {
       return;
     }
     color = gamePattern[i];
-    makeAButtonFlash(color, 600);
+    makeAButtonFlash(color, 500);
     playASound(color);
-    i++;
     await timer(1000);
   }
 }
 
 //NORMAL/STRICT TOGGLE
 
-toggleSwitch.addEventListener('click', function() {
-  if(toggleSwitch.checked) {
+toggleSwitch.addEventListener("click", function () {
+  if (toggleSwitch.checked) {
     strict = true;
-    displayText.innerHTML = 'STRICT MODE';
-    setTimeout(function() {
-      if (currentLevel === 0) {
-        displayText.innerHTML = 'PRESS START TO BEGIN';
-      } else {
-        setDefaultDisplayText();
-      }
+    const currentDisplayText =
+      displayText.innerHTML !== "WATCH!"
+        ? displayText.innerHTML
+        : (displayText.innerHTML = levelText + currentLevel);
+    displayText.innerHTML = "STRICT MODE";
+    setTimeout(function () {
+      displayText.innerHTML = currentDisplayText;
     }, 1000);
   } else {
     strict = false;
-    displayText.innerHTML = 'NORMAL MODE';
-    setTimeout(function() {
-      if (currentLevel === 0) {
-        displayText.innerHTML = 'PRESS START TO BEGIN';
-      } else {
-        setDefaultDisplayText();
-      }
+    const currentDisplayText =
+      displayText.innerHTML !== "WATCH!"
+        ? displayText.innerHTML
+        : (displayText.innerHTML = levelText + currentLevel);
+    displayText.innerHTML = "NORMAL MODE";
+    setTimeout(function () {
+      displayText.innerHTML = currentDisplayText;
     }, 1000);
   }
 });
@@ -206,30 +206,30 @@ toggleSwitch.addEventListener('click', function() {
 //CALLED BACK FUNCTIONS-------------------------------------------------------------
 
 function powerOnAnimation(time) {
-  displayAnimation('fade-in', time);
+  textAnimation("fade-in", time);
   makeAllBtnsFlash(1500);
 }
 
 function powerOffAnimation(time) {
-  displayAnimation('fade-out', time);
+  textAnimation("fade-out", time);
 }
 
-function displayAnimation(animation, time) {
-  if (animation === 'fade-in') {
-    displayText.innerHTML = 'HELLO!';
-    displayText.classList.toggle('display-animation-fade-in');
-    displayText.style.visibility = 'visible';
+function textAnimation(animationType, time) {
+  if (animationType === "fade-in") {
+    displayText.innerHTML = "WELCOME";
+    displayText.classList.toggle("display-animation-fade-in");
+    displayText.style.visibility = "visible";
     setTimeout(function () {
-      displayText.innerHTML = 'PRESS START TO BEGIN';
+      displayText.innerHTML = pressStart;
     }, time);
     setTimeout(function () {
-      displayText.classList.toggle('display-animation-fade-in');
+      displayText.classList.toggle("display-animation-fade-in");
     }, time);
   } else {
-    displayText.innerHTML = 'SHUTTING DOWN :(';
-    displayText.classList.toggle('display-animation-fade-out');
+    displayText.innerHTML = "SHUTTING DOWN :(";
+    displayText.classList.toggle("display-animation-fade-out");
     setTimeout(function () {
-      displayText.style.visibility = 'hidden';
+      displayText.style.visibility = "hidden";
     }, time);
   }
 }
@@ -238,59 +238,58 @@ function makeAllBtnsFlash(time) {
   for (let i = 0; i <= 3; i++) {
     document
       .getElementById(coloredBtnsArr[i])
-      .classList.toggle('colored-btn-on');
+      .classList.toggle("colored-btn-on");
     setTimeout(function () {
       document
         .getElementById(coloredBtnsArr[i])
-        .classList.toggle('colored-btn-on');
+        .classList.toggle("colored-btn-on");
     }, time);
   }
 }
 
 function makeAButtonFlash(coloredBtn, time) {
-  document.getElementById(coloredBtn).classList.toggle('colored-btn-on');
+  document.getElementById(coloredBtn).classList.toggle("colored-btn-on");
   setTimeout(function () {
-    document.getElementById(coloredBtn).classList.toggle('colored-btn-on');
+    document.getElementById(coloredBtn).classList.toggle("colored-btn-on");
   }, time);
 }
 
 function playASound(color) {
-  const colorAudio = new Audio('assets/sounds/' + color + '.mp3');
+  const colorAudio = new Audio("assets/sounds/" + color + ".mp3");
   colorAudio.play();
 }
 
 function turnLedLightOn(color) {
-  if (typeof(ledLight.classList[1]) === 'undefinded') {
-    ledLight.classList.add('led-light-' + color);
+  if (typeof ledLight.classList[1] === "undefinded") {
+    ledLight.classList.add("led-light-" + color);
   } else {
     turnLedLightOff();
-    ledLight.classList.add('led-light-' + color);
+    ledLight.classList.add("led-light-" + color);
   }
 }
 
 function turnLedLightOff() {
-  ledColorToBeRemove = ledLight.classList[1]; 
+  ledColorToBeRemove = ledLight.classList[1];
   ledLight.classList.remove(ledColorToBeRemove);
 }
 
 function makeLedLightFlash(color, time) {
   turnLedLightOff();
-  ledLight.classList.toggle('led-light-' + color);
-  setTimeout(function() {
-    ledLight.classList.toggle('led-light-' + color);
-  }, time)
+  ledLight.classList.toggle("led-light-" + color);
+  setTimeout(function () {
+    ledLight.classList.toggle("led-light-" + color);
+  }, time);
 }
 
 function setDefaultDisplayText() {
-  displayText.innerHTML = displayText.innerHTML = displayLevel;
-  document.getElementById('display-number').innerHTML = currentLevel;
+  displayText.innerHTML = levelText + currentLevel;
 }
 
 function gameOver() {
-  displayText.innerHTML = 'GAME OVER';
-  turnLedLightOn('red');
+  displayText.innerHTML = "GAME OVER";
+  turnLedLightOn("red");
   setTimeout(function () {
-    displayText.innerHTML = 'PRESS START TO BEGIN';
+    displayText.innerHTML = pressStart;
   }, 1000);
 }
 
@@ -300,5 +299,5 @@ function resetGame() {
   gamePattern = [];
   playerPattern = [];
   isGameStarted = false;
-  turnLedLightOff();
+  turnLedLightOn("green");
 }
